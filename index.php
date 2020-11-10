abc6866c3ff475b7646d3c8f56740ef36558e1b2
qasim-mughal <qasim.csengineer@gmail.com>
2020-11-10 06:54:55 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress Theme</title>
+</head>
+<body>
+    <h1>Qasim</h1>
+</body>
+</html>
